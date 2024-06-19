ef36738233a03076b74c702033bfe621c5c1a1b5
MUHAMMAD AYOUB <mayoub7114@gmail.com>
2024-06-19 10:20:49 +0500

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  //Port
+  PORT: process.env.PORT || 5000,
+
+  //Secret key for API
+  SECRET_KEY: "9a8s7dfg98as7dfg9a87sdfg98a7sdfg",
+
+  //Gmail credentials for send email
+  EMAIL: "dev@bistal.com",
+  PASSWORD: "ViviLive1520@",
+
+  //Secret key for jwt
+  JWT_SECRET: "YkY4fai9sUT",
+
+  //baseURL
+  baseURL: "https://the-app-backend-c2780c2bf830.herokuapp.com/",
+
+  //Firebase server key for send notification
+  SERVER_KEY: "406ff1e96700f11d905b02829f51e07a75a12960",
+};
---
+++ util/checkAccess.js
@@ -0,0 +1,22 @@
+const config = require("../config");
+
+module.exports = () => {
+  return (req, res, next) => {
+    next()
+    const token = req.headers.key || req.body.key || req.query.key;
+
+    // if (token) {
+    //   if (token == config.SECRET_KEY) {
+    //     next();
+    //   } else {
+    //     return res
+    //       .status(401)
+    //       .json({ status: false, error: "Unauthorized Access!!" });
+    //   }
+    // } else {
+    //   return res
+    //     .status(401)
+    //     .json({ status: false, error: "Unauthorized Access!!" });
+    // }
+  };
+};
